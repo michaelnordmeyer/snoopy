7baf9f5629802ac65d78d8abf571f64f494b7199
Michael Nordmeyer <michaelnordmeyer@users.noreply.github.com>
2025-05-04 19:25:35 +0200

    Bump version

--- src/snoopy.js
+++ src/snoopy.js
@@ -10,7 +10,7 @@
 
   var config = {
     NAME: 'Snoopy',
-    VERSION: '0.6.5',
+    VERSION: '0.6.6',
     URL: 'https://github.com/michaelnordmeyer/snoopy',
     CREATED: 'Created by <a href="http://allmarkedup.com/">Mark Perkins</a> and <a href="https://michaelnordmeyer.com/">Michael Nordmeyer</a>'
   };
